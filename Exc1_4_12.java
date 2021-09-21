95db158d925f9f0448d7bbcce84c5da6c8b0812f
ajrpTec <ajrp@tec.dk>
2021-09-21 10:47:31 +0200

    ...

---
+++ Exc1_4_12.java
@@ -0,0 +1,18 @@
+public class Exc1_4_12{
+
+  public static void main(String[] args){
+
+      int[] list1    = {1,2,3,4,5,6,7,8,9,10};//new int[n];
+      int[] list2    = {1,2,3,4,5,6,7,8,9,10};
+
+      //1. Opret i1 = 0 og i2 = 0
+      //2. Sammenlign list1[I1] > list2[i2]
+      //  3. Hvis ja : udskriv list[i1] og i1++, hvis i1 < n
+      //  4. Hvis nej : udskriv list[i2] og i2++, hvis i2 < n
+      //5. Hvis i1 < n eller i2 < n gentag fra 2...
+
+      //Tid 2*n ??
+
+  }
+
+}
