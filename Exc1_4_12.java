71a0eab14d4424e3d7b868f7b81d7ee6289e57e9
ajrpTec <ajrp@tec.dk>
2021-09-21 17:17:34 +0200

    Update Exc1_4_12.java

--- Exc1_4_12.java
+++ Exc1_4_12.java
@@ -5,6 +5,12 @@ public class Exc1_4_12{
       int[] list1    = {1,2,3,4,5,6,7,8,9,10};//new int[n];
       int[] list2    = {1,2,3,4,5,6,7,8,9,10};
 
+      for(int i: list1){
+        if(i == binarySearch(list, i)){
+          System.out.println(i);
+        }
+      }
+
       //1. Opret i1 = 0 og i2 = 0
       //2. Sammenlign list1[I1] > list2[i2]
       //  3. Hvis ja : udskriv list[i1] og i1++, hvis i1 < n
@@ -12,6 +18,29 @@ public class Exc1_4_12{
       //5. Hvis i1 < n eller i2 < n gentag fra 2...
 
       //Tid 2*n ??
+
+  }
+
+  public static int binarySearch(int[] list, int valueTofind){
+
+    int low         = 0;
+    int high        = list.length;
+
+    int guessIndex  = (high - low)/2;
+
+    while(high > low){
+      if(list[guessIndex] == valueTofind){ //afprøv
+        return guessIndex;
+      }
+      if(list[guessIndex] < valueTofind){
+        low = guessIndex;
+      }
+      if(list[guessIndex] > valueTofind){
+        high = guessIndex;
+      }
+    }
+
+    return -1;
 
   }
 
